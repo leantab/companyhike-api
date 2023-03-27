9deb166a5de4aa28fea7a36532694aae751d5e7f
Leandro Tabaj <leandro.tabaj@ingenious.build>
2023-03-27 13:01:47 -0300

    some missing files

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './app/Http/Livewire/**/*Table.php',
+        './vendor/power-components/livewire-powergrid/resources/views/**/*.php',
+        './vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php',
+    ],
+
+    darkMode: 'class',
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [
+        require("@tailwindcss/forms")({
+            strategy: 'class',
+        }),
+        require('@tailwindcss/typography')
+    ],
+};
